6c32d9b3f4526d4b6b1da84b07df057b56ee907e
Rafael Dominguez <venccsralph@gmail.com>
2011-09-15 09:55:45 -0430

    Set var default value in DrawPNG function.

--- functions/DrawPNG.fnc.php
+++ functions/DrawPNG.fnc.php
@@ -1,6 +1,7 @@
 <?php
 function DrawPNG($src,$extra='')
 {
+	$img = "";
 	if(strpos($_SERVER['HTTP_USER_AGENT'],"MSIE 6") || strpos($_SERVER['HTTP_USER_AGENT'], "MSIE 5.5")) 
 		$img .= "<img src=\"assets/pixel_trans.gif\" $extra style=\"filter:progid:DXImageTransform.Microsoft.AlphaImageLoader(src='assets/".$src."');\" >";
 	else
