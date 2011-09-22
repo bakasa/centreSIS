0dd06146215b4408f13d913f7491fdd699793019
Rafael Dominguez <venccsralph@gmail.com>
2011-09-22 15:41:44 -0430

    Display date of refund for a payment.

--- modules/Billing/payments.php
+++ modules/Billing/payments.php
@@ -124,6 +124,7 @@ else
 			  comment,
 		      payment_date AS payment_date,
 			  refunded,
+			  refund_date,
 			  payment_type
 			  FROM
 			  BILLING_PAYMENT
@@ -160,9 +161,10 @@ else
 			\"blank\",\"width=500,height=300\"); return false;'");
 		
 		$link['add']['html'] = array('AMOUNT'=>$buttonAdd,'PAYMENT_TYPE'=>'',
-			'PAYMENT_DATE'=>'','COMMENT'=>'','ACTION'=>'');
+			'PAYMENT_DATE'=>'','REFUND_DATE'=>'','COMMENT'=>'','ACTION'=>'');
 			
-		$columns = array('AMOUNT'=>'Amount','PAYMENT_TYPE'=>'Type','PAYMENT_DATE'=>'Date','COMMENT'=>'Comment','ACTION'=>'Action');
+		$columns = array('AMOUNT'=>'Amount','PAYMENT_TYPE'=>'Type','PAYMENT_DATE'=>'Date','REFUND_DATE'=>'Refunded Date',
+			'COMMENT'=>'Comment','ACTION'=>'Action');
 		
 		ListOutput($trans_RET,$columns,'payment','payments',$link);
 	}
